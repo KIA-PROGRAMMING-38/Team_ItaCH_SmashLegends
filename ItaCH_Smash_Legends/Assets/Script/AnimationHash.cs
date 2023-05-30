0cbfae2d6fd804115f082fd34c60ee57fb4ffe46
이찬혁 <120005279+lees5402@users.noreply.github.com>
2023-05-30 16:46:09 +0900

    Feat : Generating animated HashCodes

---
+++ ItaCH_Smash_Legends/Assets/Script/AnimationHash.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AnimationHash
+{
+    public readonly static int Idle = Animator.StringToHash("Idle");
+    public readonly static int DownIdle = Animator.StringToHash("DownIdle");
+    public readonly static int StartIdle = Animator.StringToHash("StartIdle");
+    public readonly static int Run = Animator.StringToHash("Run");
+    public readonly static int FirstAttack = Animator.StringToHash("FirstAttack");
+    public readonly static int SecondAttack = Animator.StringToHash("SecondAttack");
+    public readonly static int FinishAttack = Animator.StringToHash("FinishAttack");
+    public readonly static int HeavyAttack = Animator.StringToHash("HeavyAttack");
+    public readonly static int Jump = Animator.StringToHash("Jump");
+    public readonly static int JumpDown = Animator.StringToHash("JumpDown");
+    public readonly static int JumpLand = Animator.StringToHash("JumpLand");
+    public readonly static int JumpAttack = Animator.StringToHash("JumpAttack");
+    public readonly static int Hang = Animator.StringToHash("Hang");
+    public readonly static int HangJumpUp = Animator.StringToHash("HangJumpUp");
+    public readonly static int Hit = Animator.StringToHash("Hit");
+    public readonly static int HitUp = Animator.StringToHash("HitUp");
+    public readonly static int HitDown = Animator.StringToHash("HitDown");
+    public readonly static int RollHitUp = Animator.StringToHash("RollHitUp");
+    public readonly static int RollUpFront = Animator.StringToHash("RollUpFront");
+    public readonly static int RollUpBack = Animator.StringToHash("RollUpBack");
+    public readonly static int StandUp = Animator.StringToHash("StandUp");
+    public readonly static int Die = Animator.StringToHash("Die");
+    
+}
