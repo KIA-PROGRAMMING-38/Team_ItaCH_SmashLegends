6fc465f036eecd70aeda4207f3d78e57307a4930
Wave_Sound <120005251+wave-sound@users.noreply.github.com>
2023-06-08 18:03:03 +0900

    [Feat] Add camera stack to show UI clearly

--- ItaCH_Smash_Legends/Assets/UI/Script/LegendUI.cs
+++ ItaCH_Smash_Legends/Assets/UI/Script/LegendUI.cs
@@ -1,12 +1,14 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LegendUI : MonoBehaviour
 {
     [SerializeField] private float _heightOffset;
     private Transform _playerTransform;
+    private CharacterStatus _playerStatus;
     private float _healthPointBarFillAmount;
     private float _healthPointBarScale;
     private string _healthAmountText;
@@ -21,6 +23,12 @@ public class LegendUI : MonoBehaviour
     public void SetTransform(Transform playerTransform)
     {
         _playerTransform = playerTransform;
+        CharacterStatus characterStatus = playerTransform.GetComponent<CharacterStatus>();
+        if (characterStatus == null)
+        {
+            characterStatus = playerTransform.AddComponent<CharacterStatus>();
+        }
+        _playerStatus = characterStatus;
     }
     public void SetHealthPointBar(int HealthPoint)
     {
