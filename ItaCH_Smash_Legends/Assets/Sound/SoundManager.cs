725db94bf79631af5bc2a5266dba52af10a0d605
wave-sound <lunar105630@gmail.com>
2023-06-16 19:34:44 +0900

    [Feat] Add AudioMixer

--- ItaCH_Smash_Legends/Assets/Sound/SoundManager.cs
+++ ItaCH_Smash_Legends/Assets/Sound/SoundManager.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.Text;
+using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class SoundManager : MonoBehaviour
     private AudioSource[] _audioSources = new AudioSource[(int)SoundType.NumOfSoundType];
     private Dictionary<string, AudioClip> _audioClips = new Dictionary<string, AudioClip>();
     private StringBuilder _stringBuilder;
+    private AudioMixer _audioMixer;
 
     //테스트 코드. 추후 위치 변경 예정
     private void Awake()
@@ -26,17 +28,21 @@ public class SoundManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+
+        _audioMixer = Resources.Load<AudioMixer>("Sound/AudioMixer");
+        _stringBuilder = new StringBuilder();
+
         for (int i = 0; i < (int)SoundType.NumOfSoundType; i++)
         {
             GameObject audioSource = new GameObject { };
-            audioSource.name = Enum.GetName(typeof(SoundType), i);
+            string soundType = Enum.GetName(typeof(SoundType), i);
+            audioSource.name = soundType;
             _audioSources[i] = audioSource.AddComponent<AudioSource>();
             audioSource.transform.parent = transform;
+            _audioSources[i].outputAudioMixerGroup = _audioMixer.FindMatchingGroups(soundType)[0];
         }
 
         _audioSources[(int)SoundType.BGM].loop = true;
-        _stringBuilder = new StringBuilder();
-
     }
     public void Clear()
     {
@@ -69,16 +75,16 @@ public class SoundManager : MonoBehaviour
     {
         _stringBuilder.Clear();
         _stringBuilder.Append("Sound/");
-        if (soundType == SoundType.BGM)
-        {
-            _stringBuilder.Append("BGM/");
-        }
-        else
-        {
-            _stringBuilder.Append("SFX/");
-        }
+        _stringBuilder.Append(soundType);
+        _stringBuilder.Append("/");
         _stringBuilder.Append(name);
         AudioClip audioClip = Resources.Load<AudioClip>(_stringBuilder.ToString());
         Play(audioClip, soundType);
+    }
+
+    public void SetVolume(SoundType soundType, float value)
+    {
+        //가장 많이 사용하는 볼륨 변경 식. 원래는 기울기로 50이 아닌 20을 사용하나, 변화가 뚜렷하지 않아 50을 사용함.
+        _audioMixer.SetFloat(soundType.ToString(), Mathf.Log10(value) * 50);
     }
 }
