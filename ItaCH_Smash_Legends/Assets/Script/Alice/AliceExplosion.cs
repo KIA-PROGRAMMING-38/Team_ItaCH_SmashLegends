8bfc92c54a326cfd2b92e2eda6ad3c1463b9c9c9
이찬혁 <120005279+lees5402@users.noreply.github.com>
2023-06-19 14:41:55 +0900

    [Feat] Implement Alice hit animation

---
+++ ItaCH_Smash_Legends/Assets/Script/Alice/AliceExplosion.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AliceExplosion : MonoBehaviour
+{
+    private PlayerHit _playerHit;
+    void Start()
+    {
+        _playerHit= GetComponent<PlayerHit>();
+    }
+    
+    private void OnParticleCollision(GameObject other)
+    {
+        float heavyKnockbackPower = 0.8f;
+
+        Debug.Log("µé¾î¿È");
+        AliceGetHit(heavyKnockbackPower, AnimationHash.Hit, other);
+    }
+
+    private void AliceGetHit(float power, int animationHash, GameObject other)
+    {
+        Vector3 knockbackDirection = transform.up;
+        Rigidbody rigidbody = other.GetComponent<Rigidbody>();
+        Animator animator = other.GetComponent<Animator>();
+        //CharacterStatus opponentCharacter = GetComponent<CharacterStatus>();
+
+        rigidbody.AddForce(knockbackDirection * power, ForceMode.Impulse);
+        animator.SetTrigger(animationHash);
+        //opponentCharacter.GetDamage(damage);
+    }
+}
