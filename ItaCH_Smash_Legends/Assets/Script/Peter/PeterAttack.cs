2b4edd1c212969290608dafadff5bab5ede63454
이찬혁 <120005279+lees5402@users.noreply.github.com>
2023-06-21 00:57:27 +0900

    [Fix] Modify Peter's Skill Attack State

--- ItaCH_Smash_Legends/Assets/Script/Peter/PeterAttack.cs
+++ ItaCH_Smash_Legends/Assets/Script/Peter/PeterAttack.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class PeterAttack : PlayerAttack
 {
+    private float _skillAttackMoveSpeed = 7f;
     public override void AttackOnDash()
     {
         rigidbodyAttack.AddForce(transform.forward * defaultDashPower, ForceMode.Impulse);
     }
 
+    private void Update()
+    {
+        if(playerStatus.CurrentState == PlayerStatus.State.SkillAttack)
+        {
+            rigidbodyAttack.velocity = transform.forward * _skillAttackMoveSpeed;
+            if(playerStatus.CurrentState == PlayerStatus.State.SkillEndAttack)
+            {
+                rigidbodyAttack.velocity = Vector3.zero;
+            }
+        }
+    }
     public override void DefaultAttack()
     {
         if (IsPossibleFirstAttack())
@@ -25,6 +37,16 @@ public class PeterAttack : PlayerAttack
         {
             isFinishAttack = true;
             playerStatus.CurrentState = PlayerStatus.State.FinishComboAttack;
+        }
+    }
+
+    public override void SkillAttack()
+    {
+        if (playerStatus.CurrentState == PlayerStatus.State.Run ||
+            playerStatus.CurrentState == PlayerStatus.State.Idle)
+        {
+            animator.Play(AnimationHash.SkillAttack);
+            playerStatus.CurrentState = PlayerStatus.State.SkillAttack;
         }
     }
 }
