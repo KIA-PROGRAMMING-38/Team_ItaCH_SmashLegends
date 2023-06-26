7bf1716fefc178e0896c492b33340b08df8efb2c
이찬혁 <120005279+lees5402@users.noreply.github.com>
2023-06-26 15:56:14 +0900

    [Fix] Fixed Peter and Alice characters going into HoldIdle after a skill attack

--- ItaCH_Smash_Legends/Assets/Script/Player_State/PlayerIdleState.cs
+++ ItaCH_Smash_Legends/Assets/Script/Player_State/PlayerIdleState.cs
@@ -4,14 +4,12 @@ public class PlayerIdleState : StateMachineBehaviour
 {
     private PlayerStatus _playerStatus;
     private PlayerMove _playerMove;
-    private PlayerHit _playerHit;
     private PlayerAttack _playerAttack;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         _playerMove = animator.GetComponent<PlayerMove>();
         _playerStatus = animator.GetComponent<PlayerStatus>();
-        _playerHit = animator.GetComponent<PlayerHit>();
         _playerAttack = animator.GetComponent<PlayerAttack>();
 
         _playerStatus.CurrentState = PlayerStatus.State.Idle;
@@ -20,6 +18,7 @@ public class PlayerIdleState : StateMachineBehaviour
         _playerStatus.IsHang = false;
 
         _playerAttack.CurrentPossibleComboCount = _playerAttack.MAX_POSSIBLE_ATTACK_COUNT;
+
         if (_playerMove.moveDirection != Vector3.zero)
         {
             animator.SetBool(AnimationHash.Run, true);
--- ItaCH_Smash_Legends/Assets/Script/Player_State/PlayerSkillAttackState.cs
+++ ItaCH_Smash_Legends/Assets/Script/Player_State/PlayerSkillAttackState.cs
@@ -10,15 +10,12 @@ public class PlayerSkillAttackState : StateMachineBehaviour
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.35f)
-        {
-            _playerStatus.CurrentState = PlayerStatus.State.SkillEndAttack;
-        }
+        
     }
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        
     }
 
 }
