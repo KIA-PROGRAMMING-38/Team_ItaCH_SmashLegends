9181df20afafb33172d78c160549353365da3624
이찬혁 <120005279+lees5402@users.noreply.github.com>
2023-06-29 16:36:33 +0900

    [Fix] Alice Heavy Attack Collider Fixes and Code Fixes

--- ItaCH_Smash_Legends/Assets/Script/Util/CharacterTestModule.cs
+++ ItaCH_Smash_Legends/Assets/Script/Util/CharacterTestModule.cs
@@ -13,4 +13,12 @@ public class CharacterTestModule : MonoBehaviour
         characterStatus.InitCharacterType(characterStatus.CharacterType);
         characterStatus.InitStatus();
     }
+    // 앨리스Bomb Hit 관련 임시 코드
+    //private void BombHit(Collider other, int AnimationHash)
+    //{
+    //    Rigidbody rigidbody = other.GetComponent<Rigidbody>();
+    //    Animator animator = other.GetComponent<Animator>();
+    //    rigidbody.AddForce(_knockBackDirection * _knockBackPower, ForceMode.Impulse);
+    //    animator.SetTrigger(AnimationHash);
+    //}
 }
