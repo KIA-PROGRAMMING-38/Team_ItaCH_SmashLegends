9be6b777f670eac2dd8bfd903f5cdb56f4dd5cd8
wave-sound <120005251+wave-sound@users.noreply.github.com>
2023-07-03 15:46:10 +0900

    #11300 (#353)
    * [Feat] Add method for gradual change
    
    * [Feat] Add effect to HealthBar
    
    * [Chore] Delete test file

--- ItaCH_Smash_Legends/Assets/Script/UI/HealthBar.cs
+++ ItaCH_Smash_Legends/Assets/Script/UI/HealthBar.cs
@@ -1,18 +1,55 @@
+using Cysharp.Threading.Tasks;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
+using Util.Method;
 
 public class HealthBar : MonoBehaviour
 {
     private Image _filling;
+    private Image _fillingBackground;
+    private bool _isSetHealthPointCalledBefore;
+    private CancellationTokenSource _cancellationTokenSource;
 
+    private void Start()
+    {
+        InitHealthBarSettings();
+    }
     public void InitHealthBarSettings()
     {
-        _filling = transform.GetChild(0).GetComponent<Image>();
+        _fillingBackground = transform.GetChild(0).GetComponent<Image>();
+        _filling = transform.GetChild(1).GetComponent<Image>();
+        _isSetHealthPointCalledBefore = false;
     }
 
-    public void SetHealthPoint(int healthPoint, int healthPointPercent)
+    public async void SetHealthPoint(int healthPoint, int healthPointPercent)
     {
+        if (_isSetHealthPointCalledBefore)
+        {
+            _cancellationTokenSource?.Cancel();
+        }
+
+        // 단순히 피를 회복하여 풀피가 되는 상황을 제외하기 위해 500으로 설정.
+        if (healthPointPercent >= 500f)
+        {
+            await Method.ChangeFillAmountGradually(1, 0.5f, _filling);
+            healthPointPercent = 100;
+            _fillingBackground.fillAmount = 1;
+        }
         float healthPointRatio = healthPointPercent * 0.01f;
         _filling.fillAmount = healthPointRatio;
+
+        _cancellationTokenSource = new CancellationTokenSource();
+        _isSetHealthPointCalledBefore = true;
+
+        var WaitTask = WaitForDamage(healthPointRatio, _cancellationTokenSource.Token);
+        WaitTask.Forget();
+    }
+
+    private async UniTask WaitForDamage(float healthPointRatio, CancellationToken cancellationToken)
+    {
+        await UniTask.Delay(1000);
+        cancellationToken.ThrowIfCancellationRequested();
+        await Method.ChangeFillAmountGradually(healthPointRatio, 0.5f, _fillingBackground);
     }
 }
--- ItaCH_Smash_Legends/Assets/Script/Util/Method.cs
+++ ItaCH_Smash_Legends/Assets/Script/Util/Method.cs
@@ -1,6 +1,7 @@
 using Cysharp.Threading.Tasks;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Util.Method
 {
@@ -38,6 +39,20 @@ namespace Util.Method
                 rectTransform.Rotate(direction * (turnSpeed * Time.fixedDeltaTime));
                 await UniTask.DelayFrame(1);
             }
+        }
+
+        public static async UniTask ChangeFillAmountGradually(float targetValue, float targetTime, Image image)
+        {
+            float elapsedTime = 0;
+            float startValue = image.fillAmount;
+            while (elapsedTime <= targetTime)
+            {
+                float currentValueRatio = Mathf.Clamp01(elapsedTime / targetTime);
+                image.fillAmount = Mathf.Lerp(startValue, targetValue, currentValueRatio);
+                elapsedTime += Time.deltaTime;
+                await UniTask.DelayFrame(1);
+            }
+            image.fillAmount = targetValue;
         }
     }
 }
