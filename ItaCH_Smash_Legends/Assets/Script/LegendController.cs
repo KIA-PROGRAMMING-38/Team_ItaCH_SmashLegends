1b207b73600019b718cf88bca6a0a26da7a495fc
LeeJinWoo <krebye@naver.com>
2023-07-12 16:08:04 +0900

    [Feat] Modify Legend DownIdle and RollUp
    바뀐 시스템을 기반으로 다운 아이들 상태 및 롤업을 위한 기능 구현 및 적용 작업입니다.

--- ItaCH_Smash_Legends/Assets/Script/LegendController.cs
+++ ItaCH_Smash_Legends/Assets/Script/LegendController.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -23,10 +24,12 @@ public enum KnockbackType
 }
 public class LegendController : MonoBehaviour
 {
+    // 추후 Character base 데이터 로 변환해야함.
     private float _jumpAcceleration = 14.2f;
     private float _gravitationalAcceleration = 36f;
     private float _maxFallingSpeed = 23f;
     public static readonly float MAX_JUMP_POWER = 1f;
+    //
 
     [SerializeField] private SphereCollider _skillAttackHitZone;
     [SerializeField] private SphereCollider _attackHitZone;
@@ -40,13 +43,14 @@ public class LegendController : MonoBehaviour
     private UnityEngine.InputSystem.PlayerInput _input;
     private PlayerHit _playerHit;
     private LegendAnimationController _legendAnimationController;
-    public Vector3 MoveDirection { get; private set; }
+    private EffectController _effectController;
 
-    internal bool isJump = true;
+    public Vector3 MoveDirection { get; private set; }
+    public Vector3 RollingForward { get; private set; }
+
+    private float _rollingDashPower = 1.2f;
     private bool _canAttack;
 
-    // 추후 Character base 데이터 로 변환해야함.
-    //private const float MAX_JUMP_POWER = 1f;
     private readonly Vector3 JUMP_DIRECTION = Vector3.up;
 
     private void Awake()
@@ -62,6 +66,8 @@ public class LegendController : MonoBehaviour
     {
         FixedMaxFallSpeed();
     }
+
+    #region Input Event
     private void OnMove(InputValue value)
     {
         Vector2 input = value.Get<Vector2>();
@@ -71,7 +77,6 @@ public class LegendController : MonoBehaviour
             MoveDirection = new Vector3(input.x, 0, input.y);
         }
     }
-
     private void OnJump()
     {
         _legendAnimationController.Animator.SetTrigger(AnimationHash.Jump);
@@ -88,66 +93,14 @@ public class LegendController : MonoBehaviour
     {
         _legendAnimationController.Animator.SetTrigger(AnimationHash.SkillAttack);
     }
-    private void InitComponent()
-    {
-        _rigidbody = GetComponent<Rigidbody>();
-        _input = GetComponent<UnityEngine.InputSystem.PlayerInput>();
-        _characterStatus = GetComponent<CharacterStatus>();
-        _legendAnimationController = GetComponent<LegendAnimationController>();
-        _playerHit = GetComponent<PlayerHit>();
-    }
-    private void InitActions()
-    {
-        _actions = new InputAction[StringLiteral.Actions.Length];
+    #endregion
 
-        for (int i = 0; i < _actions.Length; ++i)
-        {
-            _actions[i] = _input.actions[StringLiteral.Actions[i]];
-        }
-    }
-
-    public bool IsTriggered(ActionType actionType) => _actions[(int)actionType].triggered;
-    public void PlayComboAttack(ComboAttackType comboAttackType)
-    {
-        if (_canAttack && IsTriggered(ActionType.DefaultAttack))
-        {
-            _legendAnimationController.AttackAnimation(comboAttackType);
-            LookForwardOnAttack();
-            _canAttack = false;
-        }
-    }
-    public void SetComboPossibleOnAnimationEvent(ComboAttackType type)
-    {
-        _legendAnimationController.SetNextAnimationClip(type);
-        _canAttack = true;
-    }
-    public void SetComboImpossibleOnAnimationEvent() => _canAttack = false;
-    private void LookForwardOnAttack()
-    {
-        if (MoveDirection != Vector3.zero)
-        {
-            transform.forward = MoveDirection;
-        }
-    }
-    public void MoveAndRotate()
-    {
-        if (MoveDirection != Vector3.zero)
-        {
-            _legendAnimationController.Animator.SetBool(AnimationHash.Run, true);
-            transform.rotation = Quaternion.LookRotation(MoveDirection);
-            transform.Translate(Vector3.forward * (5.3f * Time.deltaTime));
-        }
-        else
-        {
-            _legendAnimationController.Animator.SetBool(AnimationHash.Run, false);
-        }
-    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag(StringLiteral.Ground))
         {
             _legendAnimationController.Animator.SetBool(AnimationHash.JumpDown, false);
-            isJump = true;
+            _legendAnimationController.Animator.SetTrigger(AnimationHash.HitDown);
         }
     }
     private void OnTriggerEnter(Collider other)
@@ -160,11 +113,33 @@ public class LegendController : MonoBehaviour
 
         if (other.CompareTag(StringLiteral.DefaultHit))
         {
+            RollingForward = -1 * other.transform.forward;
             _playerHit.GetKnockbackOnAttack(other, AnimationHash.Hit, KnockbackType.Default);
         }
+
         if (other.CompareTag(StringLiteral.HeavyHit))
         {
+            RollingForward = -1 * other.transform.forward;
             _playerHit.GetKnockbackOnAttack(other, AnimationHash.HitUp, KnockbackType.Heavy);
+        }
+    }
+
+    private void InitComponent()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _input = GetComponent<UnityEngine.InputSystem.PlayerInput>();
+        _characterStatus = GetComponent<CharacterStatus>();
+        _legendAnimationController = GetComponent<LegendAnimationController>();
+        _playerHit = GetComponent<PlayerHit>();
+        _effectController = GetComponent<EffectController>();
+    }
+    private void InitActions()
+    {
+        _actions = new InputAction[StringLiteral.Actions.Length];
+
+        for (int i = 0; i < _actions.Length; ++i)
+        {
+            _actions[i] = _input.actions[StringLiteral.Actions[i]];
         }
     }
     private void FixedMaxFallSpeed()
@@ -180,8 +155,53 @@ public class LegendController : MonoBehaviour
             }
         }
     }
+
+    public void PlayComboAttack(ComboAttackType comboAttackType)
+    {
+        if (_canAttack && IsTriggered(ActionType.DefaultAttack))
+        {
+            _legendAnimationController.AttackAnimation(comboAttackType);
+            LookForwardOnAttack();
+            _canAttack = false;
+        }
+    }
+    public void SetComboPossibleOnAnimationEvent(ComboAttackType type)
+    {
+        _legendAnimationController.SetNextAnimationClip(type);
+        _canAttack = true;
+    }
+    public void SetComboImpossibleOnAnimationEvent() => _canAttack = false;
+    public void MoveAndRotate()
+    {
+        if (MoveDirection != Vector3.zero)
+        {
+            _legendAnimationController.Animator.SetBool(AnimationHash.Run, true);
+            transform.rotation = Quaternion.LookRotation(MoveDirection);
+            //transform.Translate(Vector3.forward * (_characterStatus.Stat.MoveSpeed * Time.deltaTime));
+            // 데이터 연동 전 임시 코드
+            transform.Translate(Vector3.forward * (5.3f * Time.deltaTime));
+        }
+        else
+        {
+            _legendAnimationController.Animator.SetBool(AnimationHash.Run, false);
+        }
+    }
+    private void LookForwardOnAttack()
+    {
+        if (MoveDirection != Vector3.zero)
+        {
+            transform.forward = MoveDirection;
+        }
+    }
+
+    public bool IsTriggered(ActionType actionType) => _actions[(int)actionType].triggered;
     public bool IsFalling() => _rigidbody.velocity.y <= -1f;
-    
+    public bool IsJumpOnHitUp() => _rigidbody.velocity.y <= -5f;
+
+    public void ResetVelocity()
+    {
+        _rigidbody.velocity = Vector3.zero;
+    }
     public void OnJumping()
     {
         _rigidbody.AddForce(JUMP_DIRECTION * MAX_JUMP_POWER, ForceMode.Impulse);
@@ -197,8 +217,72 @@ public class LegendController : MonoBehaviour
     private void EnableSkillAttackHitZone() => _skillAttackHitZone.enabled = true;
     private void DisableSkillAttackHitZone() => _skillAttackHitZone.enabled = false;
     #endregion
-    // PlayerRollUp 
-    // DownIdle 상태에서 기능 수행
+
+    #region Legend DownIdle State
+    public async UniTaskVoid RollingInputWaitTask()
+    {
+        await UniTask.WaitUntil(() => MoveDirection != Vector3.zero);
+
+        RollingDirection();
+        _effectController.StartInvincibleFlashEffet(_effectController.FLASH_COUNT).Forget();
+    }
+    public void RollingDash()
+    {
+        _rigidbody.AddForce(MoveDirection * _rollingDashPower, ForceMode.Impulse);
+    }
+    private void RollingDirection()
+    {
+        if (MoveDirection != Vector3.zero)
+        {
+            if (MoveDirection.x != 0 && MoveDirection.z != 0)
+            {
+                SetDiagonalRolling();
+                return;
+            }
+            else if (RollingForward == MoveDirection)
+            {
+                transform.forward = -1 * MoveDirection;
+                _legendAnimationController.Animator.SetTrigger(AnimationHash.RollUpBack);
+                return;
+            }
+            else
+            {
+                _legendAnimationController.Animator.SetTrigger(AnimationHash.RollUpFront);
+                transform.forward = MoveDirection;
+                return;
+            }
+        }
+    }
+    private void SetDiagonalRolling()
+    {
+        if (RollingForward.x > 0)
+        {
+            if (MoveDirection.x > 0)
+            {
+                transform.forward = -1 * MoveDirection;
+                _legendAnimationController.Animator.SetTrigger(AnimationHash.RollUpBack);
+            }
+            else
+            {
+                transform.forward = MoveDirection;
+                _legendAnimationController.Animator.SetTrigger(AnimationHash.RollUpFront);
+            }
+        }
+        else
+        {
+            if (MoveDirection.x < 0)
+            {
+                transform.forward = -1 * MoveDirection;
+                _legendAnimationController.Animator.SetTrigger(AnimationHash.RollUpBack);
+            }
+            else
+            {
+                transform.forward = MoveDirection;
+                _legendAnimationController.Animator.SetTrigger(AnimationHash.RollUpFront);
+            }
+        }
+    }
+    #endregion
 
     // PlayerHangController
     // Hang 상태에서 입력시 처리
--- ItaCH_Smash_Legends/Assets/Script/Player_State/LegendDownIdleState.cs
+++ ItaCH_Smash_Legends/Assets/Script/Player_State/LegendDownIdleState.cs
@@ -2,19 +2,9 @@ using UnityEngine;
 
 public class LegendDownIdleState : LegendBaseState
 {
-
-    // LegnedController 완료시 리펙토링
-
-    PlayerStatus _playerStatus;
-    PlayerRollUp _playerRollUp;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        base.OnStateEnter(animator, stateInfo, layerIndex);
-        _playerRollUp = animator.GetComponent<PlayerRollUp>();
-        _playerStatus = animator.GetComponent<PlayerStatus>();
-
-        _playerStatus.CurrentState = PlayerStatus.State.DownIdle;
-        _playerStatus.IsRollUp = true;
-        _playerRollUp.RoiingInputWaitTask().Forget();
+        base.OnStateEnter(animator,stateInfo, layerIndex);
+        legendController.RollingInputWaitTask().Forget();
     }
 }
