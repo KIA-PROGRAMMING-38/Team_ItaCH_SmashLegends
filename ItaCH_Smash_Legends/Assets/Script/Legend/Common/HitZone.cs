b077d1a2d6d3bbb9610663d3cf8b906ae1870fde
LeeJinWoo <krebye@naver.com>
2023-08-19 10:48:48 +0900

    [Feat] Legend Hit Damage Logic and Die
    레전드가 피격 데미지 및 사망 구현

--- ItaCH_Smash_Legends/Assets/Script/Legend/Common/HitZone.cs
+++ ItaCH_Smash_Legends/Assets/Script/Legend/Common/HitZone.cs
@@ -12,21 +12,10 @@ public class HitZone : MonoBehaviour
     public void SetKnockback(Rigidbody rigidbody)
     {
         rigidbody.velocity = Vector3.zero;
-        Vector3 direction = knockbackDirection;
-        rigidbody.AddForce(direction * knockbackPower, ForceMode.Impulse);
+        rigidbody.AddForce(knockbackDirection * knockbackPower, ForceMode.Impulse);
     }
     protected void Awake()
     {
         legendController = transform.GetComponentInParent<LegendController>();
-    }
-    // TODO : 스탯 연동 후 GetDamage() => legendController.Stat.HP -= damageAmount;
-    public int GetDamage()
-    {
-        return DamageAmount;
-    }
-
-    public int GetAnimationKind()
-    {
-        return AnimationType;
     }
 }
--- ItaCH_Smash_Legends/Assets/Script/Stage/DeadZone.cs
+++ ItaCH_Smash_Legends/Assets/Script/Stage/DeadZone.cs
@@ -5,6 +5,6 @@ public class DeadZone : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         var legend = other.GetComponent<LegendController>();
-        //legend?.Die(); TO DO : Die() 구현
+        legend.gameObject.SetActive(false);
     }
 }
